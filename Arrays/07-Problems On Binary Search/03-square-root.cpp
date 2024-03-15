492161ced78901f132f2c1c27198b06d1a9d4999
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2024-03-15 08:21:27 +0530

    Question Of Binary Search

---
+++ Arrays/07-Problems On Binary Search/03-square-root.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int sqrtBinarySearch(int x)
+{
+
+    int start = 0;
+    int end = x;
+
+    int ans = -1;
+
+    while (start <= end)
+    {
+
+        int mid = start + (end - start) / 2;
+
+        int sqrt = mid * mid;
+
+        if (sqrt == x)
+        {
+            ans = mid;
+            return ans;
+        }
+        else if (sqrt > x)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+
+    int x = 100;
+
+    int ans = sqrtBinarySearch(x);
+
+    cout << "Sqrt root is :: " << ans << endl;
+
+    return 0;
+}
