d63d07b24270ede6927b6588d2ede36ecc33e869
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2024-03-16 16:48:23 +0530

    Peak Element in Rotated Array

--- Arrays/08-Problems On Binary Search/02-minimun-in-rotated-array.cpp
+++ Arrays/08-Problems On Binary Search/02-minimun-in-rotated-array.cpp
@@ -6,38 +6,35 @@ int findPeakElement(int arr[], int n)
 
     int start = 0;
     int end = n - 1;
+    int ans = -1;
 
     while (start <= end)
     {
 
-        // int mid = start + (end - start) / 2;
-        int mid = end + (start - end) / 2; // make sure
+        int mid = start + (end - start) / 2;
 
-        if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid - 1])
-        {
-            return mid;
-        }
-        else if (arr[mid] > arr[mid - 1])
+        if (arr[mid] >= arr[0])
         {
             start = mid + 1;
         }
         else
         {
+            ans = arr[mid];
             end = mid - 1;
         }
     }
-    return -1;
+    return ans;
 }
 
 int main()
 {
 
-    int arr[] = {2, 4, 5, 8, 10, 8, 5, 3, 1};
+    int arr[] = {4, 6, 8, 1, 2};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     int ans = findPeakElement(arr, n);
 
-    cout << "Peak element :: " << ans << endl;
+    cout << ans << endl;
 
     return 0;
 }
