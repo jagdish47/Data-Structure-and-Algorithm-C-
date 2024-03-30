9458e07627deac80fb77b5c98ce35fdb1755d2bd
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2024-03-30 22:26:42 +0530

    factorial

---
+++ Recursion/03-Problems on Recursion/01-factorial.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int findFactorial(int num)
+{
+
+    if (num == 1)
+    {
+        return 1;
+    }
+
+    return num * findFactorial(num - 1);
+}
+
+int main()
+{
+
+    int num = 5;
+
+    int ans = findFactorial(num);
+
+    cout << ans << endl;
+
+    return 0;
+}
